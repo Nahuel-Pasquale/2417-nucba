c706b142f764f95b05890f24a81ca02a371e3cb2
npasquale <nahuel.pasquale@medicus.com.ar>
2022-08-31 21:26:26 -0300

    clase 7

---
+++ 2417-js/07-array-de-objetos/index.js
@@ -0,0 +1,61 @@
+const arr = [1, 2, 3, 4, 5];
+const arrStrings = ['gato', 'hola', 'chau', 'perro'];
+const nuevoArray = arr.concat(arrStrings)
+// console.log(nuevoString)
+// console.log(arrStrings.indexOf('hola'))
+// console.log(arrStrings.pop())
+// console.log(arrStrings.push('conejo'))
+// console.log(arrStrings.push('cajita'))
+// console.log(arrStrings.shift())
+// console.log(arrStrings.unshift('conejo'))
+// console.log(arrStrings.reverse())
+// console.log(arrStrings.join(' '))
+// console.log(arr.concat(arrStrings))
+// console.log(miChocolate.slice(0,10))
+// console.log(arrStrings.pop())
+// console.log(arrStrings.pop())
+// a) milkaoreoBlanco-64/08/2022.pdf
+// b) MILKAOREOBLANCO.pdf
+// c) milka oreo Blanco.pdf
+
+// let stringSinCosasRaras = miChocolate.split('_')
+// const formato = miChocolate.split('.').pop();
+// stringSinCosasRaras = stringSinCosasRaras[0];
+// stringSinCosasRaras = stringSinCosasRaras.split(' ').join('')
+// stringSinCosasRaras = stringSinCosasRaras.replaceAll(' ', '');
+// stringSinCosasRaras = stringSinCosasRaras.toUpperCase() + '.' + formato
+// stringSinCosasRaras = stringSinCosasRaras + '.' + formato
+
+// TAREA
+// function formatName(caracter, string){
+//     return string
+//     milkaoreoBlanco-64/08/2022.pdf
+// }
+// console.log(formatName('_', miChocolate))
+
+// console.log(formato)
+// console.log(stringSinCosasRaras)
+
+const miChocolate = 'milka oreo_Blanco_aljskncjkasnciasub.pdf'
+const str = 'guiso de nico 10'
+
+// console.log(str.repeat(2))
+// console.log(str.charAt(0))
+// console.log(str.concat(miChocolate))
+// console.log(str.includes(0))
+
+// const miPrimeraFn = (callback) => {
+//     return callback();
+// }
+// function miFn( miPrimeraFn ){
+
+// }
+
+// arr.map( function(item, index, array){} )
+// arr.filter()
+// arr.every()
+// arr.find()
+// arr.forEach()
+// arr.reduce()
+// arr.some()
+// arr.sort()
